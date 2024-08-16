decd76b44c34c1987b6b044911bff42a2047ab01
sajidcodess <muhammadsajidkhan616@gmail.com>
2024-08-17 03:54:31 +0500

    refactor

--- app/controllers/blog.controllers/deleteBlog.js
+++ app/controllers/blog.controllers/deleteBlog.js
@@ -3,22 +3,19 @@ const blogModel = require("../../models/blog.model");
 const deleteBlog = async (req, res) => {
   try {
     const { id } = req.params;
-    const isBlogExist = await blogModel.exists({ _id: id });
-    if (!isBlogExist) {
-      return res
-        .status(404)
-        .json({
-          success: false,
-          message: "The blog with this ID is not found",
-        });
-    }
-    await blogModel.findByIdAndDelete(id);
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "The blog has been successfully deleted",
+    const blog = await blogModel.findByIdAndDelete(id);
+
+    if (!blog) {
+      return res.status(404).json({
+        success: false,
+        message: "The blog with this ID is not found",
       });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "The blog has been successfully deleted",
+    });
   } catch (error) {
     res.status(400).json({ success: false, error: error.messaga });
   }
--- app/tests/blog.test.js
+++ app/tests/blog.test.js
@@ -14,10 +14,7 @@ describe("POST /api/blog/create", () => {
   it("Should Create A Post", async () => {
     const res = await request(app)
       .post("/api/blog/create")
-      .set(
-        "Authorization",
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2YmY0MTE0YWQ3N2ZiOGYzYzM0MmYyYiIsImlhdCI6MTcyMzg0NDYwNywiZXhwIjoxNzIzOTMxMDA3fQ._6YvfCMGGG-7pE0xUo84JO3bQc2KXsx0Pj48H7P3lf8",
-      )
+      .set("Authorization", `Bearer ${process.env.MY_JWT}`)
       .send({
         title: "string title goes here",
         auther: "Auther name goes here",
